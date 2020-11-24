2bc33860913f8f90ea508a252acc778e7c43a765
Saurabh Sharma <saurabhshcs@gmail.com>
2020-11-24 00:47:28 +0000

    First initial commit

---
+++ src/main/java/com/techsharezone/java8lambdas/Java8LambdasApplication.java
@@ -0,0 +1,13 @@
+package com.techsharezone.java8lambdas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8LambdasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8LambdasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techsharezone/java8lambdas/Java8LambdasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techsharezone.java8lambdas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8LambdasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
